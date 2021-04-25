8ddff1fac288e4cee9e3b8ce240a915dce8842a9
n_tananh <tananh691@gmail.com>
2021-04-26 02:33:25 +0700

    update <solution> LeetCode_1

--- Include/LeetCode/Easy/__18_1_TwoSum.py
+++ Include/LeetCode/Easy/__18_1_TwoSum.py
@@ -1,10 +1,20 @@
+# def twoSum(nums, target):
+#     i = 0
+#     for i in range(len(nums) - 1):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+#     i += 1
+
+# One pass solution __Dictionary
 def twoSum(nums, target):
-    i = 0
-    for i in range(len(nums) - 1):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    i += 1
+    dic = dict()
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in dic.keys():
+            return [dic[complement], i]
+        dic[nums[i]] = i
+
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = 9
